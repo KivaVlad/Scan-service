1d3847e54e863eac4760a403e2d33d5ff451ba7b
Владислав Кива <melnikovv717@gmail.com>
2023-06-25 22:25:34 +0300

    update code

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -169,20 +169,6 @@ const SearchForm = (props) => {
       })
     }
 
-    
-    const [inn] = useState('');
-    const [tonality] = useState("any");
-    const [limit] = useState('');
-    const [maxFullness] = useState(true);
-    const [onlyMainRole] = useState(true);
-    const [inBusinessNews] = useState(true);
-    const [onlyWithRiskFactors] = useState(false);
-    const [isTechNews] = useState(false);
-    const [isAnnouncement] = useState(true);
-    const [isDigest] = useState(false);
-    const [startDate] = useState('');
-    const [endDate] = useState('');
-
     const errorSearchFormStyle = {
         fontSize: '10px',
         color: '#FF5959',
@@ -202,7 +188,7 @@ const SearchForm = (props) => {
 
                         <h3 className='search_form_text'>ИНН компании* 7710137066</h3>
                         <input className="search_form_input"
-                            {...register("inn",{ required: true, minLength: 10, maxLength: 10, value: inn })} 
+                            {...register("inn",{ required: true, minLength: 10, maxLength: 10 })} 
                             aria-invalid={errors.inn ? "true" : "false"} 
                             type='number'
                             placeholder="10 цифр"
@@ -215,7 +201,7 @@ const SearchForm = (props) => {
 
                         <h3 className='search_form_text'>Тональность</h3>
                         <select className="search_form_input"
-                            {...register("tonality", {value: tonality})}>
+                            {...register("tonality")}>
                             <option value="any">Любая</option>
                             <option value="positive">Позитивная</option>
                             <option value="negative">Негативная</option>
@@ -224,7 +210,7 @@ const SearchForm = (props) => {
 
                         <h3 className='search_form_text'>Количество документов в выдаче*</h3>
                         <input className="search_form_input"
-                            {...register("limit", { required: true, minLength: 1, maxLength: 1000, value: limit })} 
+                            {...register("limit", { required: true, minLength: 1, maxLength: 1000 })} 
                             aria-invalid={errors.limit ? "true" : "false"} 
                             type='number'
                             placeholder="От 1 до 1000"
@@ -240,13 +226,13 @@ const SearchForm = (props) => {
                     </div>
 
                     <div className="search_wrapper_checkboxs">
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("maxFullness", {value: maxFullness})}/><label className="search_checkbox_text">Признак максимальной полноты</label></div>
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("inBusinessNews", {value: inBusinessNews})}/><label className="search_checkbox_text">Упоминания в бизнес-контексте</label></div>
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("onlyMainRole", {value: onlyMainRole})}/><label className="search_checkbox_text">Главная роль в публикации</label></div>
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("onlyWithRiskFactors", {value: onlyWithRiskFactors})}/><label className="search_checkbox_text">Публикации только с риск-факторами</label></div>
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("includeTechNews", {value: isTechNews})}/><label className="search_checkbox_text">Включать технические новости рынков</label></div>
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("includeAnnouncements", {value: isAnnouncement})}/><label className="search_checkbox_text">Включать анонсы и календари</label></div>
-                        <div className="search_checkbox_box"><input type="checkbox" {...register("includeDigests", {value: isDigest})}/><label className="search_checkbox_text">Включать сводки новостей</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" checked={true} {...register("maxFullness")}/><label className="search_checkbox_text">Признак максимальной полноты</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" checked={true} {...register("inBusinessNews")}/><label className="search_checkbox_text">Упоминания в бизнес-контексте</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" checked={true} {...register("onlyMainRole")}/><label className="search_checkbox_text">Главная роль в публикации</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" {...register("onlyWithRiskFactors")}/><label className="search_checkbox_text">Публикации только с риск-факторами</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" {...register("includeTechNews")}/><label className="search_checkbox_text">Включать технические новости рынков</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" checked={true} {...register("includeAnnouncements")}/><label className="search_checkbox_text">Включать анонсы и календари</label></div>
+                        <div className="search_checkbox_box"><input type="checkbox" {...register("includeDigests")}/><label className="search_checkbox_text">Включать сводки новостей</label></div>
                     </div>
                 </div>
 
@@ -254,14 +240,14 @@ const SearchForm = (props) => {
                     <div>
                         <div className="search_data_container_inputs">
                             <input className="search_form_data_input"
-                                {...register("startDate", { required: true, value: startDate })} 
+                                {...register("startDate", { required: true })} 
                                 aria-invalid={errors.startDate ? "true" : "false"} 
                                 type='date'
                                 placeholder="Дата начала"
                             />
 
                             <input className="search_form_data_input"
-                                {...register("endDate", { required: true, value: endDate })} 
+                                {...register("endDate", { required: true })} 
                                 aria-invalid={errors.endDate ? "true" : "false"} 
                                 type='date'
                                 placeholder="Дата конца"
