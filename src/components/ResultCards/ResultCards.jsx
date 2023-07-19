f7471985cb9304736adbc2a138b8d777c44ff47d
Владислав Кива <melnikovv717@gmail.com>
2023-07-19 23:38:16 +0300

    Изменил режим отображения даты и названия источника

--- src/components/ResultCards/ResultCards.jsx
+++ src/components/ResultCards/ResultCards.jsx
@@ -9,28 +9,26 @@ const ResultCards = ({ doc }) => {
 
     return(
             <div className="documents_card">
+
                 <div className="documents_card_header">
-                    <div className="documents_card_date">{doc.issueDate.substr(0,10)}</div>
-                    <div className="documents_card_sourse">{doc.source.name.substr(0, 30)} ...</div>
+                    <div className="documents_card_date">{doc.issueDate.substr(0,10).split('-').reverse().join('-')}</div>
+                    <div className="documents_card_sourse">{doc.source.name}</div>
                 </div>
+
                 <div className="documents_card_title">{doc.title.text.substr(0, 70)} ...</div>
                 <div className="documents_card_tag">Технические новости</div>
-
-                {Boolean(bgUrl) ? (
-                    <div 
-                        className="documents_card_image"
-                        style={{ backgroundImage: `url(${bgUrl})` }}
-                    ></div>
-                )
-                :
+                {Boolean(bgUrl) ?
+                    <div className="documents_card_image" style={{ backgroundImage: `url(${bgUrl})` }}></div>
+                    :
                     <div className="documents_card_image"></div>
                 }
-
                 <div className="documents_card_text" dangerouslySetInnerHTML={{ __html: content }} />
+
                 <div className="documents_card_footer">
                     <Link target='_blank' to={doc.url} className="documents_card_button">Читать в источнике</Link>
                     <span className="documents_card_words">{doc.attributes.wordCount} слов(а)</span>
                 </div>
+
             </div>
     )
 }
